732181a87ba8e6e993feab65f3734a0cdb0a2b5d
Alfons Flystam <alfons.flystam@hotmail.com>
2019-02-08 13:55:18 +0100

    Fungerar hyfsat

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RestSharp;
+using PokeAPI;
 
 namespace API
 {
@@ -12,43 +13,19 @@ namespace API
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("http://api.nobelprize.org/v1/laureate.json");
-            var request = new RestRequest("/", Method.GET);
-            IRestResponse response = client.Execute(request);
-            String content = response.Content;
-            Winners winners = JsonConvert.DeserializeObject<Winners>(content);
+            Pokemon(1);
+            Console.Read();
         }
 
-        public class Prize
+        async static void Pokemon(int pokeID)
         {
-            public string year { get; set; }
-            public string category { get; set; }
-            public string share { get; set; }
-            public string motivation { get; set; }
-            public List<object> affiliations { get; set; }
-            public string overallMotivation { get; set; }
-        }
-
-        public class Laureate
-        {
-            public string id { get; set; }
-            public string firstname { get; set; }
-            public string surname { get; set; }
-            public string born { get; set; }
-            public string died { get; set; }
-            public string bornCountry { get; set; }
-            public string bornCountryCode { get; set; }
-            public string bornCity { get; set; }
-            public string diedCountry { get; set; }
-            public string diedCountryCode { get; set; }
-            public string diedCity { get; set; }
-            public string gender { get; set; }
-            public List<Prize> prizes { get; set; }
-        }
-
-        public class Winners
-        {
-            public List<Laureate> laureates { get; set; }
+            EvolutionChain e = await DataFetcher.GetApiObject<EvolutionChain>(pokeID);
+            PokemonSpecies p = await DataFetcher.GetApiObject<PokemonSpecies>(pokeID);
+            Console.WriteLine(e.Chain.Species.Name);
+            foreach (var item in e.Chain.EvolvesTo)
+            {
+                Console.WriteLine(item.Species.Name);
+            }
         }
     }
 }
